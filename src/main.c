c5046f7bc09703448be96906d87edea3c8373e29
Quentin FISCH <quentin.fisch@epita.fr>
2020-10-06 15:21:45 +0200

    comment gtk includes

--- src/main.c
+++ src/main.c
@@ -203,16 +203,16 @@ int main1() {
 	return 0;
 }
 
-static void
-activate (GtkApplication* app)
-{
-  GtkWidget *window;
+// static void
+// activate (GtkApplication* app)
+// {
+//   GtkWidget *window;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
-}
+//   window = gtk_application_window_new (app);
+//   gtk_window_set_title (GTK_WINDOW (window), "Window");
+//   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+//   gtk_widget_show_all (window);
+// }
 
 int main(int argc, char **argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
