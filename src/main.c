1e1bbe096238030e2f7ba02ce0c0817b74ad19fe
charles.simon-meunier <charles.simon-meunier@epita.fr>
2020-11-28 22:36:43 +0100

    .

--- src/main.c
+++ src/main.c
@@ -255,6 +255,109 @@ int remove_directory(const char *path) {
    return r;
 }
 
+
+
+// INTERFACE
+
+int columns = 1;
+int contrast = 0;
+int noise = 1;
+int correction = 1;
+
+GtkBuilder *builder;
+SDL_Surface *img;
+char *filename;
+
+typedef struct {
+    GtkWidget *errorImageLabel;
+    GtkWidget *resultLabel;
+} app_widgets;
+
+app_widgets        *widgets;
+
+void interface()
+{
+    widgets = g_slice_new(app_widgets);
+    gtk_init(NULL, NULL);
+    GtkBuilder *builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "src/Interface/interface.glade", NULL);
+    GtkWidget       *window= GTK_WIDGET(gtk_builder_get_object(builder, "main"));
+    widgets->errorImageLabel  = GTK_WIDGET(gtk_builder_get_object(builder, "errorImage"));
+    widgets->resultLabel  = GTK_WIDGET(gtk_builder_get_object(builder, "resultOutput"));
+    
+    gtk_builder_connect_signals(builder, widgets);
+
+    gtk_widget_show(window);
+    g_object_unref(builder);
+
+    gtk_main();
+
+    printf("%i|%i|%i|%i", columns, contrast, noise, correction);
+}
+
+// called when window is closed
+void on_destroy()
+{
+    gtk_main_quit();
+}
+
+void file_set (GtkFileChooser *file_chooser, gpointer data)
+{
+    filename = gtk_file_chooser_get_preview_filename (file_chooser);
+    img = SDL_LoadBMP(filename);
+    if (!img) {
+        gtk_label_set_text(GTK_LABEL(widgets->errorImageLabel), "We cant't load your image. Please make sur that this is a .bmp extension");
+    }
+    else {
+        gtk_label_set_text(GTK_LABEL(widgets->errorImageLabel), "");
+    }
+}
+
+
+void toggleColumns()
+{
+    columns = !columns;
+}
+
+void toggleContrast()
+{
+    contrast = !contrast;
+}
+
+void toggleNoise()
+{
+    noise = !noise;
+}
+
+void toggleCorrection()
+{
+    correction = !correction;
+}
+
+
+void launchRecognition()
+{
+    if (img) {
+		grayscale(img, 1, "results/temp.bmp");
+		img = blackAndWhite(img, 1, "results/temp.bmp");
+        paragraphSegmentation(filename, "results/test", 1);
+        char *result = "bite";
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (widgets->resultLabel));
+        gtk_text_buffer_set_text (buffer, result, -1);
+    }
+    else {
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (widgets->resultLabel));
+        gtk_text_buffer_set_text (buffer, "Not good", -1);
+    }
+}
+
+
+
+// END INTERFACE
+
+
+
+
 int main(int argc, char **argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	if (argc < 2) {
--- src/main.h
+++ src/main.h
@@ -11,7 +11,6 @@
 #include <dirent.h>
 #include <gtk-3.0/gtk/gtk.h>
 #include "ImageTreatment/filters.h"
-#include "Interface/interface.h"
 
 
 
